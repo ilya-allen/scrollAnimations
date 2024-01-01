39d1214245e329001c55a59220595197b035b71d
realwally1 <il.yaallen2008@yahoo.com.au>
2024-01-02 08:24:30 +1300

    minor update

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 'use strict';
 
+history.scrollRestoration = 'manual';
 const image = document.querySelector('.sam');
 const headerText = document.querySelector('.header_text')
 const body = document.querySelector('body')
 let scrollVal = 0;
 
-window.addEventListener("wheel", function() {
+window.onbeforeunload = () => {
+    window.scrollTo(0, 0);
+}
 
-    image.style.transform = `translateX(${200}%)`
-    headerText.style.transform = `translateX(${-300}%)`
-});
+window.onwheel = e => {
+    
+    if(e.deltaY >= 0){
+
+        scrollVal += 50;
+        image.style.transform = `translateX(${scrollVal}%)`
+        headerText.style.transform = `translateX(-${scrollVal}%)`
+    } else {
+        if(window.scrollY > 0) {
+            console.log(e.deltaY)
+            console.log(body.offsetTop)
+            scrollVal -= 50;
+            image.style.transform = `translateX(${scrollVal}%)`
+            headerText.style.transform = `translateX(-${scrollVal}%)`
+        }
+    }
+}
