710984c8205938fdb8be1dc0a562b241d7c76e48
realwally1 <il.yaallen2008@yahoo.com.au>
2024-01-03 10:24:00 +1300

    full rework styles

--- index.js
+++ index.js
@@ -3,27 +3,14 @@
 history.scrollRestoration = 'manual';
 const image = document.querySelector('.sam');
 const headerText = document.querySelector('.header_text')
+const downButton = document.querySelector('.down_button')
 const body = document.querySelector('body')
+const bodySection = document.querySelector('.body_section')
 let scrollVal = 0;
 
-window.onbeforeunload = () => {
-    window.scrollTo(0, 0);
-}
-
-window.onwheel = e => {
-    
-    if(e.deltaY >= 0){
-
-        scrollVal += 50;
-        image.style.transform = `translateX(${scrollVal}%)`
-        headerText.style.transform = `translateX(-${scrollVal}%)`
-    } else {
-        if(window.scrollY > 0) {
-            console.log(e.deltaY)
-            console.log(body.offsetTop)
-            scrollVal -= 50;
-            image.style.transform = `translateX(${scrollVal}%)`
-            headerText.style.transform = `translateX(-${scrollVal}%)`
-        }
-    }
-}
+downButton.addEventListener('click', function() {
+    bodySection.scrollIntoView();
+    downButton.style.transform = 'translateX(-400%)'
+    headerText.style.transform = 'translateX(-200%)'
+    image.style.transform = 'translateX(200%)'
+})
